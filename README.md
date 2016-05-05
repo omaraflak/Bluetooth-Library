0323b7bb2f6ec5bbdbfdc665a4896ec66055c374
omaflak <omaflak@yahoo.fr>
2016-05-05 17:08:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# Important
+I used a BufferedReader to read data from the bluetooth socket. As I'm reading with readLine(), each message you're sending to the Android must end with a \n. Otherwise it won't be received.
+
+The class uses listeners so everything is really easy! Enjoy :)
+
+# Install
+
+Add to your gradle dependencies:
+
+	compile 'me.aflak.bluetooth-library:bluetooth-library:1.1'
+
+## In your Activity
+
+	Bluetooth bluetooth = new Bluetooth();
+	bluetooth.enableBluetooth();
+	
+## Listener to scan and pair
+
+	bluetooth.setDiscoveryCallback(new Bluetooth.DiscoveryCallback() {
+	
+	      @Override
+	      public void onFinish() {
+	            // scan finished
+	      }
+	      
+	      @Override
+	      public void onDevice(BluetoothDevice device) {
+	            // device found
+	      }
+	      
+	      @Override
+	      public void onPair(BluetoothDevice device) {
+	            // device paired
+	      }
+	      
+	      @Override
+	      public void onUnpair(BluetoothDevice device) {
+	            // device unpaired
+	      }
+	      
+	      @Override
+	      public void onError(String message) {
+	            // error occurred
+	      }
+	});
+	
+## Scan and Pair
+
+	bluetooth.scanDevices();
+	bluetooth.pair(device);
+	
+## Listener to communicate
+
+	bluetooth.setCommunicationCallback(new Bluetooth.CommunicationCallback() {
+	      @Override
+	      public void onConnect(BluetoothDevice device) {
+	              // device connected
+	      }
+	      
+	      @Override
+	      public void onDisconnect(BluetoothDevice device, String message) {
+	            // device disconnected
+	      }
+	      
+	      @Override
+	      public void onMessage(String message) {
+	            // message received (it has to end with a \n to be received)
+	      }
+	      
+	      @Override
+	      public void onError(String message) {
+	            // error occurred 
+	      }
+	      
+	      @Override
+	      public void onConnectError(BluetoothDevice device, String message) {
+	            // error during connection
+	      }
+	});
+	
+## Connect to device
+	
+	// three options
+	bluetooth.connectToName("name");
+	bluetooth.connectToAddress("address");
+	bluetooth.connectToDevice(device);
+	
+## Send a message
+
+	bluetooth.send("message");
+	
+## Get paired devices
+
+	List<BluetoothDevice> devices = bluetooth.getPairedDevices();
---
+++ bluetooth/src/main/java/me/aflak/bluetooth/Bluetooth.java
@@ -0,0 +1,284 @@
+package me.aflak.bluetooth;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Omar on 14/07/2015.
+ */
+public class Bluetooth {
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket socket;
+    private BluetoothDevice device, devicePair;
+    private BufferedReader input;
+    private OutputStream out;
+
+    private boolean connected=false;
+    private CommunicationCallback communicationCallback=null;
+    private DiscoveryCallback discoveryCallback=null;
+
+    private Activity activity;
+
+    public Bluetooth(Activity activity){
+        this.activity=activity;
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void enableBluetooth(){
+        if(bluetoothAdapter!=null) {
+            if (!bluetoothAdapter.isEnabled()) {
+                bluetoothAdapter.enable();
+            }
+        }
+    }
+
+    public void disableBluetooth(){
+        if(bluetoothAdapter!=null) {
+            if (bluetoothAdapter.isEnabled()) {
+                bluetoothAdapter.disable();
+            }
+        }
+    }
+
+    public void connectToAddress(String address) {
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+        new ConnectThread(device).start();
+    }
+
+    public void connectToName(String name) {
+        for (BluetoothDevice blueDevice : bluetoothAdapter.getBondedDevices()) {
+            if (blueDevice.getName().equals(name)) {
+                connectToAddress(blueDevice.getAddress());
+                return;
+            }
+        }
+    }
+
+    public void connectToDevice(BluetoothDevice device){
+        new ConnectThread(device).start();
+    }
+
+    public void disconnect() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            if(communicationCallback!=null)
+                communicationCallback.onError(e.getMessage());
+        }
+    }
+
+    public boolean isConnected(){
+        return connected;
+    }
+
+    public void send(String msg){
+        try {
+            out.write(msg.getBytes());
+        } catch (IOException e) {
+            connected=false;
+            if(communicationCallback!=null)
+                communicationCallback.onDisconnect(device, e.getMessage());
+        }
+    }
+
+    private class ReceiveThread extends Thread implements Runnable{
+        public void run(){
+            String msg;
+            try {
+                while ((msg = input.readLine()) != null) {
+                    if (communicationCallback != null)
+                        communicationCallback.onMessage(msg);
+                }
+            } catch (IOException e) {
+                connected=false;
+                if (communicationCallback != null)
+                    communicationCallback.onDisconnect(device, e.getMessage());
+            }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        public ConnectThread(BluetoothDevice device) {
+            Bluetooth.this.device=device;
+            try {
+                Bluetooth.this.socket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                if(communicationCallback!=null)
+                    communicationCallback.onError(e.getMessage());
+            }
+        }
+
+        public void run() {
+            bluetoothAdapter.cancelDiscovery();
+
+            try {
+                socket.connect();
+                out = socket.getOutputStream();
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                connected=true;
+
+                new ReceiveThread().start();
+
+                if(communicationCallback!=null)
+                    communicationCallback.onConnect(device);
+            } catch (IOException e) {
+                if(communicationCallback!=null)
+                    communicationCallback.onConnectError(device, e.getMessage());
+
+                try {
+                    socket.close();
+                } catch (IOException closeException) {
+                    if (communicationCallback != null)
+                        communicationCallback.onError(closeException.getMessage());
+                }
+            }
+        }
+    }
+
+    public List<BluetoothDevice> getPairedDevices(){
+        List<BluetoothDevice> devices = new ArrayList<>();
+        for (BluetoothDevice blueDevice : bluetoothAdapter.getBondedDevices()) {
+            devices.add(blueDevice);
+        }
+        return devices;
+    }
+
+    public BluetoothSocket getSocket(){
+        return socket;
+    }
+
+    public BluetoothDevice getDevice(){
+        return device;
+    }
+
+    public void scanDevices(){
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        activity.registerReceiver(mReceiverScan, filter);
+        bluetoothAdapter.startDiscovery();
+    }
+
+    public void pair(BluetoothDevice device){
+        activity.registerReceiver(mPairReceiver, new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED));
+        devicePair=device;
+        try {
+            Method method = device.getClass().getMethod("createBond", (Class[]) null);
+            method.invoke(device, (Object[]) null);
+        } catch (Exception e) {
+            if(discoveryCallback!=null)
+                discoveryCallback.onError(e.getMessage());
+        }
+    }
+
+    public void unpair(BluetoothDevice device) {
+        devicePair=device;
+        try {
+            Method method = device.getClass().getMethod("removeBond", (Class[]) null);
+            method.invoke(device, (Object[]) null);
+        } catch (Exception e) {
+            if(discoveryCallback!=null)
+                discoveryCallback.onError(e.getMessage());
+        }
+    }
+
+    private BroadcastReceiver mReceiverScan = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            switch (action) {
+                case BluetoothAdapter.ACTION_STATE_CHANGED:
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    if (state == BluetoothAdapter.STATE_OFF) {
+                        if (discoveryCallback != null)
+                            discoveryCallback.onError("Bluetooth turned off");
+                    }
+                    break;
+                case BluetoothAdapter.ACTION_DISCOVERY_FINISHED:
+                    context.unregisterReceiver(mReceiverScan);
+                    if (discoveryCallback != null)
+                        discoveryCallback.onFinish();
+                    break;
+                case BluetoothDevice.ACTION_FOUND:
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    if (discoveryCallback != null)
+                        discoveryCallback.onDevice(device);
+                    break;
+            }
+        }
+    };
+
+    private final BroadcastReceiver mPairReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_BOND_STATE_CHANGED.equals(action)) {
+                final int state = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothDevice.ERROR);
+                final int prevState	= intent.getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, BluetoothDevice.ERROR);
+
+                if (state == BluetoothDevice.BOND_BONDED && prevState == BluetoothDevice.BOND_BONDING) {
+                    if(discoveryCallback!=null)
+                        discoveryCallback.onPair(devicePair);
+                } else if (state == BluetoothDevice.BOND_NONE && prevState == BluetoothDevice.BOND_BONDED){
+                    if(discoveryCallback!=null)
+                        discoveryCallback.onUnpair(devicePair);
+                }
+            }
+        }
+    };
+
+    public interface CommunicationCallback{
+        void onConnect(BluetoothDevice device);
+        void onDisconnect(BluetoothDevice device, String message);
+        void onMessage(String message);
+        void onError(String message);
+        void onConnectError(BluetoothDevice device, String message);
+    }
+
+    public void setCommunicationCallback(CommunicationCallback communicationCallback) {
+        this.communicationCallback = communicationCallback;
+    }
+
+    public void removeCommunicationCallback(){
+        this.communicationCallback = null;
+    }
+
+    public interface DiscoveryCallback{
+        void onFinish();
+        void onDevice(BluetoothDevice device);
+        void onPair(BluetoothDevice device);
+        void onUnpair(BluetoothDevice device);
+        void onError(String message);
+    }
+
+    public void setDiscoveryCallback(DiscoveryCallback discoveryCallback){
+        this.discoveryCallback=discoveryCallback;
+    }
+
+    public void removeDiscoveryCallback(){
+        this.discoveryCallback=null;
+    }
+
+}
+
+

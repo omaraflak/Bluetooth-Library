6b295bdec363621abeafeb73972f111b04cefdfd
Omar Aflak <aflakomar@gmail.com>
2018-02-16 17:23:27 +0100

    update version

--- app/src/main/java/me/aflak/libraries/ui/chat/interactor/ChatInteractorImpl.java
+++ app/src/main/java/me/aflak/libraries/ui/chat/interactor/ChatInteractorImpl.java
@@ -31,7 +31,7 @@ public class ChatInteractorImpl implements ChatInteractor {
     @Override
     public void connectToDevice(BluetoothDevice device, CommunicationCallback callback) {
         bluetooth.setCommunicationCallback(callback);
-        bluetooth.connectToDevice(device, true);
+        bluetooth.connectToDevice(device);
     }
 
     @Override

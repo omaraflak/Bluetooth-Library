4ca44ab0497a9ec0bf927d2b33ea511400cafab8
mrobles@cedesa.es <Mrobles3>
2018-06-14 16:10:46 +0200

    Leaks detection

--- app/src/main/java/me/aflak/libraries/MyApp.java
+++ app/src/main/java/me/aflak/libraries/MyApp.java
@@ -2,6 +2,8 @@ package me.aflak.libraries;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import me.aflak.libraries.data.BluetoothModule;
 
 public class MyApp extends Application {
@@ -12,7 +14,19 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
+
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
         bluetoothModule = new BluetoothModule(this);
+
+        // Normal app init code...
+
     }
 
     public static MyApp app() {

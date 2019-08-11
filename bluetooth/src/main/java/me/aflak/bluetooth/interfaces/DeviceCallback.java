4b0dc2103dfb46d1f9a55557830524fc1b9dd7db
Omar Aflak <momaraflak@twitter.com>
2019-08-11 09:53:01 +0200

    message listener return byte array

--- bluetooth/src/main/java/me/aflak/bluetooth/interfaces/DeviceCallback.java
+++ bluetooth/src/main/java/me/aflak/bluetooth/interfaces/DeviceCallback.java
@@ -9,7 +9,7 @@ import android.bluetooth.BluetoothDevice;
 public interface DeviceCallback {
     void onDeviceConnected(BluetoothDevice device);
     void onDeviceDisconnected(BluetoothDevice device, String message);
-    void onMessage(String message);
+    void onMessage(byte[] message);
     void onError(int errorCode);
     void onConnectError(BluetoothDevice device, String message);
 }
